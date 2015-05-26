709e427ee61337b16b7185c8ef9328011445dd6e
student <student@M14048>
2015-05-26 10:45:27 +0900

    pom update

--- src/com/sds/icto/studyer/service/MemberService.java
+++ src/com/sds/icto/studyer/service/MemberService.java
@@ -1,10 +1,10 @@
 package com.sds.icto.studyer.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sds.icto.studyer.domain.MemberVo;
 import com.sds.icto.studyer.repository.MemberDao;
-
 
 @Service
 public class MemberService {
